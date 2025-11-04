d392c5a184618854d9acc8f1d872fc1595fb949a
tigersame <devssaini365@gmail.com>
2025-11-04 21:30:11 +0530

    Add missing lib/constants.ts file for APP_CONFIG

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_CONFIG = {
+  APP_NAME: 'Base Tap Game',
+  GAME_NAME: 'Base Tap Game',
+  DEFAULT_URL: 'http://localhost:3000',
+  DESCRIPTION: 'Play Tap Tap Game and earn BASETAP tokens on Base network!',
+} as const
